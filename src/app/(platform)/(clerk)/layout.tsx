a28e491126aefd608d5926ec81200292fb8cc99f
Amal221200 <amalmurikkoli2000@gmail.com>
2024-03-24 16:21:24 +0530

    clerk authentication

---
+++ src/app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,11 @@
+
+
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <main className="h-full flex items-center justify-center">
+            {children}
+        </main>
+    )
+}
+
+export default ClerkLayout
